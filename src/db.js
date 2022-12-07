c233d8754a7939b65dbb1e7e3076ca59edb7bacd
anfemece123 <90008499+anfemece123@users.noreply.github.com>
2022-12-06 20:40:47 -0500

    f

--- src/db.js
+++ src/db.js
@@ -6,8 +6,7 @@ const path = require("path");
 const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
 
 const sequelize = new Sequelize(
-  `postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{
-    PGDATABASE}}`,
+  `postgresql://postgres:Dkgg8MJnMdBtdJP9Crxj@containers-us-west-107.railway.app:6276/railway`,
 
   // postgres:Dkgg8MJnMdBtdJP9Crxj@containers-us-west-107.railway.app:6276/railway
 
